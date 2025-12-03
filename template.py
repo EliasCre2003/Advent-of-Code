97f008169f3c9a5f4693b4079697b1ac2d185b1b
Elias Cregård <eliascregard701@gmail.com>
2025-12-03 16:32:01 +0100

    updaterade kod-mallen

--- template.py
+++ template.py
@@ -1,20 +1,23 @@
 TEST = True
 
+
+def part1(data: str) -> str:
+    ...
+
+
+def part2(data: str) -> str:
+    ...
+
+
 def load_input(test: bool = False) -> str:
     with open(f'{"test" if test else "input"}.txt', 'r') as f:
         return f.read().strip()
 
 
-def part1() -> str:
-    ...
-
-
-def part2() -> str:
-    ...
-
-
 def main():
     data = load_input(TEST)
+    print(f'Part 1: {part1(data)}')
+    print(f'Part 2: {part2(data)}')
 
 
 if __name__ == '__main__':
