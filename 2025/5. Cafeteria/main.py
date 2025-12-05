48e8d78a00a7a67ca31af7990e4e794f8a335462
Elias Cregård <eliascregard701@gmail.com>
2025-12-05 15:14:43 +0100

    dag 2 del 2 2025

--- 2025/5. Cafeteria/main.py
+++ 2025/5. Cafeteria/main.py
@@ -1,4 +1,4 @@
-TEST = False
+TEST = True
 
 
 def parse_data(data: str) -> tuple[tuple[int, int], list[int]]:
@@ -19,7 +19,30 @@ def part1(data: str) -> str:
 
 
 def part2(data: str) -> str:
-    ...
+    ranges, _ = parse_data(data)
+
+    def fix_overlap(r1: tuple[int, int]) -> tuple[int, int]:
+        new_r = list(r1)
+        for r2 in ranges:
+            if r2 is None: continue
+            if r2 == r1: continue
+            if r2[0] <= new_r[0] and r2[1] >= new_r[1]:
+                return None
+            if r2[0] <= new_r[0] and new_r[0] <= r2[1] <= new_r[1]:
+                new_r[0] = r2[1] + 1
+            elif r2[1] >= new_r[1] and new_r[0] <= r2[0] <= new_r[1]:
+                new_r[1] = r2[0] - 1
+        return new_r
+    
+    def range_size(r: tuple[int, int]) -> int:
+        if r is None: return 0
+        return max(r[1] - r[0] + 1, 0)
+
+    for i in range(len(ranges)):
+        ranges[i] = fix_overlap(ranges[i])
+    
+    return sum(map(range_size, ranges))
+
 
 
 def load_input(test: bool = False) -> str:
